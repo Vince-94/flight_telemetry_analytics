cbd30e8939a7f94a5e6747689f0b36bd94c346de
Vince-94 <ruotolovincenzo94@gmail.com>
2025-12-01 21:48:30 -0600

    Small update

--- src/core/config.py
+++ src/core/config.py
@@ -5,15 +5,13 @@ import yaml
 
 
 CONFIG_FILEPATH = "src/config/config.yaml"
-
-
 with open(CONFIG_FILEPATH, "r") as file:
     config = yaml.safe_load(file)
 
 
 class Settings(BaseSettings):
     DATABASE_URL: str = config.get("DATABASE_URL")
-    REDIS_URL: str = "redis://localhost:6379/0"
+    REDIS_URL: str = config.get("REDIS_URL")
 
 @lru_cache
 def get_settings() -> Settings:
